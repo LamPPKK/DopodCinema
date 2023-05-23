9be5b38a26f18387fa9d68cd9626de42caa2a5cf
TheLN <lengocthe54@gmail.com>
2023-05-23 23:50:30 +0700

    change text

--- DopodCinema/Module/Movie/MovieDetail/MovieDetailViewController.swift
+++ DopodCinema/Module/Movie/MovieDetail/MovieDetailViewController.swift
@@ -84,7 +84,7 @@ class MovieDetailViewController: BaseViewController<MovieDetailViewModel> {
         playView.layer.cornerRadius = playView.frame.height / 2
         playView.backgroundColor = Constant.Color.color3D5BF6
         
-        playLabel.text = "Play movie"
+        playLabel.text = "Play trailers"
         playLabel.font = .fontPoppinsMedium(withSize: 13)
         playLabel.textColor = .white
         
